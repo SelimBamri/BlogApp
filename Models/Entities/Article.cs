bfaa85514fe14ab17aa0c5692ccd8566f3d6c5b2
SelimBamri <selim.bamri@ihec.u-carthage.tn>
2024-09-01 17:33:53 +0200

    Adding Article entity

---
+++ Models/Entities/Article.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.Models.Entities
+{
+    public class Article
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public required string Title { get; set; }
+        [Required]
+        public required string Description { get; set; }
+        [Required]
+        public required byte[] Banner { get; set; }
+        [Required]
+        public required string Content { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public required DateTime Created { get; set; }
+
+        public string AuthorFk { get; set; }
+        [ForeignKey("AuthorFk")]
+        public required virtual User Author { get; set; }
+
+    }
+}
